510ee0db443eb0b03addaed976760386f4ffc2f9
JingtianYan <48559888+JingtianYan@users.noreply.github.com>
2019-12-28 20:25:00 +0800

    Add files via upload

---
+++ Supermarket robot/MCU/LCD1602.h
@@ -0,0 +1,9 @@
+#ifndef __LCD1602_H_
+#define __LCD1602_H_
+
+
+
+void LCD1602Init(void);
+void LCD1602WriteCommand(char comm);
+void LCD1602WriteDistance(unsigned int distance);
+#endif
---
+++ Supermarket robot/MCU/UltrasonicCtrol.h
@@ -0,0 +1,13 @@
+#ifndef __ULTRASONICCTROL_H_
+#define __ULTRASONICCTROL_H_
+
+extern volatile unsigned int distance_cm;//µ±«∞æ‡¿Î
+void UltraSoundInit(void);
+void GetDistanceDelay(void);
+void GetDistanceDelayA(void);
+void GetDistanceDelayB(void);
+void Distance(void);
+void DistanceA(void);
+void DistanceB(void);
+void LCD12864WriteDistance(unsigned int distance);
+#endif
